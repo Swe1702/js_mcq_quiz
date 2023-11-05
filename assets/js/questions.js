4141a37f1c0ce550bb56ecb31d7f930254b26a16
Swe1702 <swethakothandan2807@gmail.com>
2023-11-05 20:20:33 +0000

    adds scores.js,quiz.js and questions.js files with questions

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+//adds questions , choices for the quiz.
+var questions = [
+  {
+    question: "Commonly used data types DO Not Include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "numbers",
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "curly brackets",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____ elements.",
+    choices: ["numbers and strings","other arrays","objects","all of the above"],
+    answer: "numbers and strings",
+  },
+    {
+       question: "What does HTML stand for?",
+       choices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Transfer Markup Language"],
+       answer: "Hyper Text Markup Language",
+    },
+    {
+       question: "What does CSS stand for?",
+       choices: ["Colorful Style Sheets", "Creative Style Sheets", "Computer Style Sheets", "Cascading Style Sheets"],
+       answer: "Cascading Style Sheets",
+    },
+    {
+       question: "What programming language is known as the 'mother of all languages'?",
+       choices: ["C", "Java", "Python", "Assembly Language"],
+       answer: "C",
+    }
+];
